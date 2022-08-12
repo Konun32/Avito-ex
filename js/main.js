7dc19d000455e5fca7941a04cffe105bf746ad47
Konun32 <aleksey.u.work@mail.ru>
2022-08-13 01:06:15 +0500

    page

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+	iconMenu.addEventListener("click", function(e){
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	})
+}
+
+//slick slider
+$(document).ready(function(){
+	$('.cases__slider').slick({
+		arrows: true,
+		dots: false,
+		centerMode: true,
+		slidesToShow: 1,
+	});
+});
