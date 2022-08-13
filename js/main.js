2691ed48b8416860d7ad4b4057067f8104d96bd3
Konun32 <aleksey.u.work@mail.ru>
2022-08-14 00:20:26 +0500

    page

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 
 const iconMenu = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.menu__body');
+const links = document.querySelectorAll('.menu__link');
 if (iconMenu) {
 	iconMenu.addEventListener("click", function(e){
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	})
+}
+
+for(let index = 0; index < links.length; index++) {
+	let link = links[index];
+	link.addEventListener("click", function(e){
 		document.body.classList.toggle('_lock');
 		iconMenu.classList.toggle('_active');
 		menuBody.classList.toggle('_active');
@@ -18,3 +28,39 @@ $(document).ready(function(){
 		slidesToShow: 1,
 	});
 });
+
+// button
+
+let buttons = document.getElementsByClassName('slid__button');
+for(let index = 0; index < buttons.length; index++) {
+	let button = buttons[index];
+	button.addEventListener("click", function(e){
+		if(button.classList.contains('slid__button_before')) {
+			if(!button.classList.contains('slid__button_active')) {
+				button.classList.add('slid__button_active');
+				button.nextElementSibling.classList.remove('slid__button_active');
+				let buttonsList = button.parentElement;
+				let imgList = buttonsList.nextElementSibling;
+				let imgBefore = imgList.firstElementChild;
+				if(!imgBefore.classList.contains('slid__button_active')) {
+					imgBefore.classList.add('slid__img_active');
+					let imgAfter = imgList.lastElementChild;
+					imgAfter.classList.remove('slid__img_active');
+				}
+			}
+		} else if(button.classList.contains('slid__button_after')) {
+			if(!button.classList.contains('slid__button_active')) {
+				button.classList.add('slid__button_active');
+				button.previousElementSibling.classList.remove('slid__button_active');
+				let buttonsList = button.parentElement;
+				let imgList = buttonsList.nextElementSibling;
+				let imgAfter = imgList.lastElementChild;
+				if(!imgAfter.classList.contains('slid__button_active')) {
+					imgAfter.classList.add('slid__img_active');
+					let imgBefore = imgList.firstElementChild;
+					imgBefore.classList.remove('slid__img_active');
+				}
+			}
+		}	
+	})
+}
